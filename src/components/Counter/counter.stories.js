00dafd1040eadea66cdc23220792d01c376b4feb
maxloboyko <loboykomax92@gmail.com>
2023-11-02 01:38:58 +0200

    added Counter component

---
+++ src/components/Counter/counter.stories.js
@@ -0,0 +1,16 @@
+import Counter from './index';
+
+export default {
+   title: 'Example/Counter',
+   component: Counter,
+   tags: ['autodocs'],
+   // parameters: {
+   //    layout: 'fullscreen',
+   // },
+};
+
+export const Income = {
+   args: {
+      onChange: () => { }
+   },
+};
---
+++ src/components/Counter/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const Counter = () => {
+   const [clicks, setClicks] = useState(0);
+   const [step, setStep] = useState(1);
+
+   const showValue = () => {
+      setTimeout(() => {
+         alert(clicks)
+      }, 3000);
+   }
+
+   return (
+      <div>
+         <div>Clicked: {clicks}</div>
+         <button onClick={() => setClicks(clicks + step)}>Click</button>
+         <br />
+         <br />
+         <button onClick={showValue}>Show Value</button>
+         <br />
+         <br />
+         <input name='step' value={step} onChange={(e) => setStep(+e.target.value)} />
+      </div>
+   )
+};
+
+export default Counter;
